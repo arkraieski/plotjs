5836458f12d75f43c771e2f59900900fd388b774
Alexander Kraieski <arkraieski@gmail.com>
2026-03-29 20:04:34 -0400

    use chart.js v4.5.1 release

--- R/utils.R
+++ R/utils.R
@@ -8,10 +8,9 @@ col2hex <- function(col) {
   )
 }
 
-plotjsSizingPolicy <- function(padding = 0, viewer.padding = 0, browser.fill = TRUE) {
+plotjsSizingPolicy <- function(padding = 8, browser.fill = TRUE) {
   htmlwidgets::sizingPolicy(
     padding = padding,
-    browser.fill = browser.fill,
-    viewer.padding = viewer.padding
+    browser.fill = browser.fill
   )
 }
